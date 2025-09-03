5748de7ec1f50d0bcbfc5d6d6a66fd1eb1d9b963
JLukoo <lijayden2020@gmail.com>
2025-09-03 19:28:39 -0400

    completed header

--- components/header.tsx
+++ components/header.tsx
@@ -35,7 +35,7 @@ export function Header() {
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center space-x-2">
           <Link href="/" className="text-xl font-bold">
-            Portfolio
+           Jayden's Portfolio
           </Link>
         </div>
 
